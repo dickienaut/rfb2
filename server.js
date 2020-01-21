70de7d4e602aa76cc25c177eec8ad73c71a692ee
Lyle Dickie <lyle.e.dickie@gmail.com>
2020-01-21 10:52:22 -0500

    Create backend routes

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const express = require('express')
-
 const app = express()
+
+// Define routes
+app.use('/api/users', require('./routes/users.js'))
+app.use('/api/contacts', require('./routes/contacts'))
+app.use('/api/contacts', require('./routes/auth'))
+
 const PORT = process.env.PORT || 5000
+
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
