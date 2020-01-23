591a053d490a284c3fcf6a22bebe1e51c9d6b40b
Lyle Dickie <lyle.e.dickie@gmail.com>
2020-01-23 10:45:07 -0500

    Updated app to provide jwt token on signup

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const config = require('config')
 const User = require('../models/User.js')
 const { check, validationResult } = require('express-validator')
 const router = express.Router();
@@ -40,7 +42,17 @@ router.post('/',
     user.password = await bcrypt.hash(password, salt)
 
     await user.save()
-    res.send('User saved')
+
+    const payload = {
+      user: {
+        id: user.id
+      }
+    }
+
+    jwt.sign(payload, config.get('jwtSecret'), { expiresIn: 36000 }, (err, token) => {
+      if(err) throw err
+      res.json({ token })
+    })
 
   } catch (err) {
     console.error(err.message)
