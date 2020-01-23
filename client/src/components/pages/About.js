37a1581d8c2fb88b7567128fafb6a325ef53d516
Lyle Dickie <lyle.e.dickie@gmail.com>
2020-01-23 15:29:11 -0500

    Add Navbar, About, Home components. Set up Router.

---
+++ client/src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+const About = () => {
+  return (
+    <div>
+      <h1>About this App</h1>
+      <p className='my-1'>
+        This is a full stack React app for keeping track of your contacts
+      </p>
+    </div>
+  );
+}
+
+export default About;
