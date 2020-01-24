e73cc1686aa6f9be8efe00ce80f8a58de8a4b160
Lyle Dickie <lyle.e.dickie@gmail.com>
2020-01-24 17:27:36 -0500

    Set up context for auth

--- client/src/App.js
+++ client/src/App.js
@@ -4,23 +4,27 @@ import Navbar from './components/layout/Navbar.js'
 import Home from './components/pages/Home.js'
 import About from './components/pages/About.js'
 import ContactState from './context/contact/ContactState.js'
+import AuthState from './context/auth/AuthState.js'
+
 import './App.css';
 
 const App = () => {
   return (
-    <ContactState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className='container'>
-            <Switch>
-              <Route exact path='/' component={Home}/>
-              <Route exact path='/about' component={About}/>
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </ContactState>
+    <AuthState>
+      <ContactState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className='container'>
+              <Switch>
+                <Route exact path='/' component={Home}/>
+                <Route exact path='/about' component={About}/>
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </ContactState>
+    </AuthState>
   );
 }
 
